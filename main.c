56e702c47c3f6fa7abf9e685c366dc1bcfca07e7
Said Alvarado-Marin <said_alvarado@hotmail.com>
2024-05-28 21:07:32 +0200

    main: add pio configuration

--- main.c
+++ main.c
@@ -4,21 +4,30 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include "hardware/pio.h"
 #include "pico/stdlib.h"
+#include "ts4231_capture.pio.h"
+#include <stdio.h>
 
-int main()
-{
-    stdio_init_all();
-    if (cyw43_arch_init())
-    {
-        printf("Wi-Fi init failed");
-        return -1;
-    }
-    while (true)
-    {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        sleep_ms(250);
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        sleep_ms(750);
-    }
+#define LH2_0_DATA_PIN 15 // The Envelope pin will be (Data pin + 1)
+
+int main() {
+
+  // Configure the PIO
+  PIO pio = pio0;
+  uint offset = pio_add_program(pio, &ts4231_capture_program);
+  uint sm = pio_claim_unused_sm(pio, true);
+  ts4231_capture_program_init(pio, sm, offset, LH2_0_DATA_PIN);
+
+  stdio_init_all();
+  if (cyw43_arch_init()) {
+    printf("Wi-Fi init failed");
+    return -1;
+  }
+  while (true) {
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    sleep_ms(250);
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+    sleep_ms(750);
+  }
 }
