d20ce50dfa0870d574e612cb3f7197e960f2521a
Said Alvarado-Marin <said_alvarado@hotmail.com>
2025-03-02 22:49:22 +0100

    32_poly: first try to add tests

---
+++ test/test_lh2.c
@@ -0,0 +1,16 @@
+
+#include "unity.h"
+// #include "lh2.h"
+
+void test_lfsr_search_poly_0(void) {
+    TEST_ASSERT_EQUAL(1,2);
+}
+
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_lfsr_search_poly_0);
+    return UNITY_END();
+}
+
+
